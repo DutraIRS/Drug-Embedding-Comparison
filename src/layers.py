7fedd9f50a48cd64e25aa57a896bc17827a2bb9a
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2025-03-19 10:53:45 -0300

    GCNConv - Refactor

--- src/layers.py
+++ src/layers.py
@@ -1,9 +1,17 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class GCNConv(nn.Module):
-    def __init__(self, input_dim, output_dim, activation = nn.ReLU()):
+    """Graph Convolutional Network Convolution Layer
+    """
+    def __init__(self, input_dim: int, output_dim: int, activation: nn.Module = nn.ReLU()):
+        """Initialize the GCNConv layer
+
+        Args:
+            input_dim (int): Number of features in input graph
+            output_dim (int): Number of features in output graph
+            activation (nn.Module, optional): Activation function. Defaults to nn.ReLU().
+        """
         super(GCNConv, self).__init__()
         
         self.input_dim = input_dim
@@ -13,7 +21,16 @@ class GCNConv(nn.Module):
         # Initialize weights from a normal distribution with 0 mean and low variance
         self.W = nn.Parameter(torch.randn([input_dim, output_dim]) / 10)
     
-    def forward(self, X, A):
+    def forward(self, X: torch.Tensor, A: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the GCNConv layer
+
+        Args:
+            X (torch.Tensor): Input graph of shape [num_nodes, input_dim]
+            A (torch.Tensor): Adjacency matrix of shape [num_nodes, num_nodes]
+
+        Returns:
+            torch.Tensor: Output graph of shape [num_nodes, output_dim]
+        """
         A += torch.eye(A.size()[0])
         
         D_tilde = torch.diag(A.sum(dim=0) ** (-1/2))
