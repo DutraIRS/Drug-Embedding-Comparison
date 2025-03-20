493f4624034c995c88005e5f6197098d294e19ca
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2025-03-19 21:46:53 -0300

    GATConv - Refactor

--- src/layers.py
+++ src/layers.py
@@ -40,7 +40,16 @@ class GCNConv(nn.Module):
         return self.activation(X_tilde)
 
 class GATConv(nn.Module):
-    def __init__(self, input_dim, output_dim, hidden_dim):
+    """Graph Self-Attention Convolutional Layer
+    """
+    def __init__(self, input_dim: int, output_dim: int, hidden_dim: int):
+        """Initialize the GATConv layer
+
+        Args:
+            input_dim (int): Number of features in input graph
+            output_dim (int): Number of features in output graph
+            hidden_dim (int): Dimension of Wq and Wk weight tensors
+        """
         super(GATConv, self).__init__()
         
         self.input_dim = input_dim
@@ -52,15 +61,38 @@ class GATConv(nn.Module):
         self.Wk = nn.Parameter(torch.randn([input_dim, hidden_dim]) / 10)
         self.Wv = nn.Parameter(torch.randn([input_dim, output_dim]) / 10)
     
-    def masked_attention(self, Q, K, V, A):
+    def masked_attention(self, Q: torch.Tensor, K: torch.Tensor, V: torch.Tensor,
+                        A: torch.Tensor) -> torch.Tensor:
+        """Perform masked attention
+        
+        No attention is paid to non-neighbor nodes.
+
+        Args:
+            Q (torch.Tensor): Queries matrix
+            K (torch.Tensor): Keys matrix
+            V (torch.Tensor): Values matrix
+            A (torch.Tensor): Adjacency matrix of shape [num_nodes, num_nodes]
+
+        Returns:
+            torch.Tensor: Update tokens embeddings
+        """
         X = Q @ K.T
         X = X + torch.nan_to_num(- torch.inf * (1 - A), nan=0) # Masking before softmax
-        X = torch.softmax(X, dim=1)
+        X = torch.softmax(X, dim=1) # -inf becomes 0 after softmax
         X = X @ V
         
         return X
     
-    def forward(self, X, A):
+    def forward(self, X: torch.Tensor, A: torch.Tensor) -> torch.Tensor:
+        """Forward pass of the GATConv layer
+
+        Args:
+            X (torch.Tensor): Input graph of shape [num_nodes, input_dim]
+            A (torch.Tensor): Adjacency matrix of shape [num_nodes, num_nodes]
+
+        Returns:
+            torch.Tensor: Output graph of shape [num_nodes, output_dim]
+        """
         A += torch.eye(A.size()[0])
         
         Q = X @ self.Wq
